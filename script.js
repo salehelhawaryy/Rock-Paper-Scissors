6563a7b3470783d14adfb9fd0d6855aea8fce76c
salehelhawaryy <salehmohamad210@gmail.com>
2023-04-14 17:44:25 +0200

    all logic complemeted

--- script.js
+++ script.js
@@ -11,17 +11,51 @@ function getComputerChoice() {
 }
 
 
-function playerSelection(player,computer) {
+function playRound(player,computer) {
   let playerUse=String(player).toLowerCase();
   let computerUse=String(computer).toLowerCase();
   if(playerUse==computerUse) 
     return "Draw! Both choices were "+playerUse;
   if(playerUse=="paper")
     {
-        if(computerUser=="rock")
-            return "You Win! Paper Beats rock"
+        if(computerUse=="rock")
+            return "You Win! Paper Beats Rock";
+        return "You Lose! Scissors Beat Paper"; 
     }
+  if(playerUse=="rock") 
+  {
+        if(computerUse=="scissors")
+            return "You Win! Rock Beats Scissors";
+        return "You Lose! Paper Beats Rock";
+  }
+  if(computerUse=="paper")
+      return "You Win! Scissors Beat Paper";
+  return "You Lose! Rock Beats Scissors";
 }
 
+function game(playerName){
+  let winsPlayer=0;
+  let winsComputer=0;
+   while(winsPlayer<3 && winsComputer<3)
+    {
+      let playerSelection= prompt(playerName+" enter your selection");
+      let res=playRound(playerSelection,getComputerChoice());
+      console.log(res);
 
-console.log(getComputerChoice());
+
+        if(res.charAt(4)=='W')
+          winsPlayer++;
+        if(res.charAt(4)=='L')
+          winsComputer++;
+      
+    }
+
+  
+  if(winsPlayer==3) 
+    console.log(playerName+" you won the best of 5 games!")
+  else console.log(playerName+ " you lose the best of 5 games :(");
+}
+
+let name=prompt("Player, Enter your name");
+game(name);
+
